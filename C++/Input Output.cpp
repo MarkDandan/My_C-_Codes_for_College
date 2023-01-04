859515df595fa1749986db183a6b2b2f13fc9e4e
MarkDandan <121887004+MarkDandan@users.noreply.github.com>
2023-01-04 11:12:47 +0800

    Create Input Output.cpp

---
+++ C++/Input Output.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string input;
+	cout << "Input a message: ";
+	getline(cin, input);
+
+	cout << endl<< input << endl;
+}
